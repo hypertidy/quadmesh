004f4fa3138548da27f45d95f9375ead5984d37b
Michael Sumner <mdsumner@gmail.com>
2018-08-08 04:09:41 +1000

    docs

--- vignettes/quadmesh.R
+++ vignettes/quadmesh.R
@@ -23,3 +23,33 @@ shade3d(qm, col = grey(scl(qm$vb[3,qm$ib])))
 
 rglwidget()
 
+## ------------------------------------------------------------------------
+(m <- matrix(1:12, nrow = 3))
+
+## ------------------------------------------------------------------------
+x <- seq(1, nrow(m)) - 0.5
+y <- seq(1, ncol(m)) - 0.5
+image(x, y, m)
+
+text(expand.grid(x, y), lab = m[])
+
+
+## ------------------------------------------------------------------------
+library(raster)
+(r <- raster(t(m[, ncol(m):1]), xmn = 0, xmx =ncol(m), ymn = 0, ymx = nrow(m)))
+
+## ------------------------------------------------------------------------
+xyz <- as.data.frame(r, xy = TRUE)
+head(xyz)
+tail(xyz)
+
+## ------------------------------------------------------------------------
+as(as(raster::rasterToPolygons(r), "SpatialLinesDataFrame"), 
+   "SpatialPointsDataFrame")
+
+
+## ------------------------------------------------------------------------
+library(quadmesh)
+qm <- quadmesh(r)
+str(qm)
+
