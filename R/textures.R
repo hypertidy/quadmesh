7646ab6ec3fa1813ecd996e7d0999c64ec6101f8
Michael Sumner <mdsumner@gmail.com>
2018-11-12 09:03:10 +0000

    tiny data

---
+++ R/textures.R
@@ -0,0 +1,17 @@
+target_coordinates <- function(xy, src.proj = NA_character_, target) {
+  dst.proj <- raster::projection(target)
+  if (!is.na(src.proj) && !is.na(src.proj)) {
+    if (raster::isLonLat(src.proj)) {
+      ## src is lonlat, so to radians
+      xy <- xy * pi / 180
+    }
+    xy <- proj4::ptransform(xy, src.proj = src.proj, dst.proj = dst.proj)
+    if (is.list(xy)) xy <- do.call(cbind, xy)[, 1:2, drop = FALSE]
+    if (raster::isLonLat(dst.proj)) {
+      xy <- xy * 180 / pi
+    }
+  }  ## otherwise we just assume they are in the same crs
+  xy
+}
+
+
--- data-raw/mapbox.R
+++ data-raw/mapbox.R
@@ -1,26 +1,6 @@
 library(ceramic)
 mapbox_sat  <- ceramic:::cc_location(cbind(137, -32), buffer = c(1e6, 5e5), type = "mapbox.satellite")
 projection(mapbox_sat) <- "+proj=merc +a=6378137 +b=6378137"
-
-usethis::use_data(mapbox_sat)
-
-
-#
-#
-#
 library(raster)
- etopo_aus <- crop(etopo, projectExtent(mapbox_sat, projection(etopo)))
- etopo_aus[etopo_aus < -1000] <- -1000
-# qm <- quadmesh(etopo_aus)
-# texture <- tempfile(fileext = ".png")
-# rgdal::writeGDAL(as(mapbox_sat, "SpatialGridDataFrame"), texture, driver = "PNG")
-#
-#
-# texcoords <- xyFromCell(setExtent(mapbox_sat, extent(0, 1, 0, 1)),
-#                         cellFromXY(mapbox_sat, rgdal::project(t(qm$vb[1:2, ]), projection(mapbox_sat))))
-#
-# library(rgl)
-# rgl.clear()
-# ## qm <- addNormals(qm)  ## consider doing this
-# shade3d(qm, texture = texture, texcoords = texcoords[qm$ib, ], col = "white")
-# aspect3d(1, 1, .2)
+mapbox_sat <- aggregate(mapbox_sat, fact = 4)
+usethis::use_data(mapbox_sat, compress = "xz")
