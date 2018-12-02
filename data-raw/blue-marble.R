b454c2bc69c422e8103db2f048e0e72fa4e288e2
Michael Sumner <mdsumner@gmail.com>
2018-12-03 09:31:49 +1100

    add tiny blue marble internal data

---
+++ data-raw/blue-marble.R
@@ -0,0 +1,12 @@
+u <- "https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73909/world.topo.bathy.200412.3x5400x2700.png"
+f <- file.path("data-raw", basename(u))
+if (!file.exists(f)) curl::curl_download(u, f)
+library(lazyraster)
+library(raster)
+dm <- c(216, 108)
+bm <- setExtent(raster::brick(as_raster(lazyraster::lazyraster(f, band = 1), dm),
+                    as_raster(lazyraster::lazyraster(f, band = 2), dm),
+                    as_raster(lazyraster::lazyraster(f, band = 3), dm,)),
+                extent(-180, 180, -90, 90))
+projection(bm) <- "+init=epsg:4326"
+usethis::use_data(bm, internal=TRUE)
