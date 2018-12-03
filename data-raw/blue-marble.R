2b48568e47ee0f72d25660a57ee1449f219049b6
Michael Sumner <mdsumner@gmail.com>
2018-12-03 17:58:48 +0000

    use raster

--- data-raw/blue-marble.R
+++ data-raw/blue-marble.R
@@ -3,10 +3,13 @@ f <- file.path("data-raw", basename(u))
 if (!file.exists(f)) curl::curl_download(u, f)
 library(lazyraster)
 library(raster)
-dm <- c(216, 108)
-bm <- setExtent(raster::brick(as_raster(lazyraster::lazyraster(f, band = 1), dm),
-                    as_raster(lazyraster::lazyraster(f, band = 2), dm),
-                    as_raster(lazyraster::lazyraster(f, band = 3), dm,)),
-                extent(-180, 180, -90, 90))
+#dm <- c(216, 108)
+#bm <- setExtent(raster::brick(as_raster(lazyraster::lazyraster(f, band = 1), dm),
+#                    as_raster(lazyraster::lazyraster(f, band = 2), dm),
+#                    as_raster(lazyraster::lazyraster(f, band = 3), dm,)),
+#                extent(-180, 180, -90, 90))
+bm <- setExtent(raster::brick(f), extent(-180, 180, -90, 90))
 projection(bm) <- "+init=epsg:4326"
+
+bm <- aggregate(bm, fact = 25)
 usethis::use_data(bm, internal=TRUE)
