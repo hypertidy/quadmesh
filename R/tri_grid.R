2152402a49ef329beca10d9fd10c791f1eeffbe1
Michael Sumner <mdsumner@gmail.com>
2018-12-11 17:02:56 +1100

    bary_index function

--- R/tri_grid.R
+++ R/tri_grid.R
@@ -1,37 +1,57 @@
 
-#' Experimental
+#' Barycentric triangle index for interpolation
 #'
-#' @param x a triangle mesh (see `triangmesh`)
-#' @param grid raster grid
+#' This function returns the barycentric weight for a grid of coordinates
+#' from a geographic raster.
+#'
+#' It's not as fast as `rasterProject(x, grid)` but it
+#' also accepts a `coords` argument and so can be used for non-regular raster
+#' reprojection.
+#'
+#' 'coords' may be 'NULL' or longitude, latitude in a 2-layer raster brick or stack as with
+#' `mesh_plot`.
+#' @param x a 'RasterLayer' source
+#' @param grid target 'RasterLayer', a target regular grid
 #' @param ... ignored
-#' @param a_srs projection that belongs to x coordinates
+#' @param coords optional input coordinates
 #'
 #' @return RasterLayer
-#' @noRd
-#'
+#' @export
+#' @importFrom geometry tsearch
 #' @examples
-#' library(raadtools)
-#' ice <- readice()
-#' ex <- extent(-180, 180, -90, -30)
-#' sst <- readsst(xylim = ex)
-#' tm <- triangmesh(sst)
-#' index <- tri_index(tm, a_srs = projection(sst), grid = ice)
+#' library(raster)
+#' p_srs <- "+proj=stere +lat_0=-90 +lat_ts=-71"
+#' polar <- raster(extent(-5e6, 5e6, -5e6, 5e6), crs = p_srs, res = 25000)
+#' etopo <- aggregate(etopo, fact = 4)
+#' index <- bary_index(etopo, grid = polar)
 #' ok <- !is.na(index$idx)
-#' r <- ice * NA
-#' files <- sstfiles()
-#' for (i in seq(1, 1000)) {
-#' r[ok] <- rowSums(matrix(values(readsst(files$date[i], inputfiles = files, xylim = ex))[tm$it[, index$idx[ok]]], ncol = 3, byrow = TRUE) * index$p[ok, ])
-#'
-#' plot(r, zlim = c(-2, 14), col = palr::sstPal(36)[1:30])
-#'}
-tri_index <- function(x, a_srs = NULL, grid = NULL, ...) {
-  xy <- t(x$vb[1:2, ])
-  if (is.null(grid)) grid <- defaultgrid(xy)
-  tri <- t(tm$it)
+#' r <- setValues(polar, NA_integer_)
+#' r[ok] <- colSums(matrix(values(etopo)[index$tri[, index$idx[ok]]], nrow = 3) * t(index$p)[, ok])
+#' plot(r)
+bary_index <- function(x, coords = NULL, grid = NULL, ...) {
+  tm <- triangmesh(x)
+  if (!is.null(coords)) {
+    xy <- raster::values(coords)
+  } else {
+    xy <- sp::coordinates(x)
+  }
+  if (is.null(grid)) grid <- default_grid(xy)
+  tri <- tm$it
+  a_srs <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
   xy <- target_coordinates(xy, a_srs, raster::projection(grid))
   value <- tm$vb[3, ]
   rxy <- sp::coordinates(grid)
-  pid0 <- geometry::tsearch(xy[,1], xy[,2], t(tm$it), coordinates(grid)[,1], coordinates(grid)[, 2],
+
+ # nn <- RANN::nn2(xy[x$it[1, ], 1:2], rxy, k = 1)
+ #  ord <- order(nn$nn.idx)
+  pid0 <- geometry::tsearch(xy[,1], xy[,2], t(tri), rxy[,1], rxy[, 2],
                             bary = TRUE)
+  pid0$tri <- tri
   pid0
+}
+
+default_grid <- function (xy, ncols = 60, nrows = 50, prj = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs +towgs84=0,0,0")
+{
+  raster::raster(raster::extent(xy), ncols = ncols, nrows = nrows,
+                 crs = prj)
 }
