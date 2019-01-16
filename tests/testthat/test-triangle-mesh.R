b7be0d1b4a271fa395278d726638b4f4bc91ae15
Michael Sumner <mdsumner@gmail.com>
2019-01-16 01:55:19 +0000

    fix test

--- tests/testthat/test-triangle-mesh.R
+++ tests/testthat/test-triangle-mesh.R
@@ -2,5 +2,8 @@ context("test-triangle-mesh")
 
 test_that("triangle mesh works", {
   ## test will change when triangle mesh is properly featured
-  triangmesh(etopo) %>% expect_s3_class("mesh3d") %>% expect_named(c("it", "vb", "primitivetype", "material"))
+  triangmesh(etopo) %>%
+    expect_s3_class("mesh3d") %>%
+    expect_s3_class("triangmesh") %>%
+    expect_named(c("it", "vb", "primitivetype", "material", "raster_metadata", "crs"))
 })
