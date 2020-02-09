f6cd1c0555e2dfac1a86d8f7e3c6554a91814f4d
Michael Sumner <mdsumner@gmail.com>
2020-02-09 10:16:31 +0000

    new mesh3d basis for raster

--- R/exy_values.R
+++ R/exy_values.R
@@ -1,11 +1,14 @@
-## from a raster, calculate values at cell corners
-## TODO: remove need for raster functions values(), dim()
-## and port into mesh3d package (allow in-mem rasters, not lazy ones)
+## from a raster, calculate values at cell corner
+## allow only 1-layer in-mem rasters, not lazy ones
 exy <- function(x, ...) {
-  v <- raster::values(x)
-  nr <- dim(x)[1]
-  nc <- dim(x)[2]
-  m <- matrix(v, nc, nr)
+  if (is.numeric(x@data@values) && length(x@data@values) > 0) {
+    nr <- x@nrows
+    nc <- x@ncols
+    m <- if (is.matrix(x@data@values)) x@data@values[,1L, drop = TRUE] else x@data@values
+    m <- matrix(m, nc, nr)
+  } else {
+    stop("raster is not in-memory, cannot obtain values - use 'raster::readAll(x[[1]])' before conversion to mesh")
+  }
   ## top left
   tl <- cbind(NA_integer_, rbind(NA_integer_, m))
   ## top right
@@ -21,14 +24,18 @@ exy <- function(x, ...) {
 }
 
 
-as_mesh3d <- function(x, ...) {
-  nc1 <- ncol(x) + 1
+as_mesh3d_raster <- function(x, ...) {
+  nc <- x@ncols
+  nc1 <- nc + 1
   aa <- t(prs(seq(nc1)))
   ind <- matrix(c(rbind(aa, aa[2:1, ])) + c(0, 0, nc1, nc1), 4)
   ind0 <- as.integer(as.vector(ind) +
                        rep(seq(0, length = nrow(x), by = ncol(x) + 1), each = 4 * dim(x)[2L]))
   ind1 <- matrix(ind0, nrow = 4)
-  rgl::qmesh3d(rbind(t(quadmesh::edgesXY(x)),  exy(x), 1),
-               ind1
+  v <- exy(x)
+  cols <- viridis::viridis(100)
+  rgl::qmesh3d(rbind(t(quadmesh::edgesXY(x)),  v, 1),
+               ind1,
+               material = list(color = cols[scales::rescale(v, to = c(1, 100))])
                )
 }
