4fee622193cfd712075638b52f41a8f6ce27d310
Michael Sumner <mdsumner@gmail.com>
2020-02-09 00:00:10 +0000

    tidy up point interpolation

--- R/exy_values.R
+++ R/exy_values.R
@@ -1,6 +1,4 @@
 exy <- function(x, ...) {
-  exy <- quadmesh:::edgesXY(x)
-
   v <- raster::values(x)
   nr <- dim(x)[1]
   nc <- dim(x)[2]
@@ -14,10 +12,23 @@ exy <- function(x, ...) {
   ## bottom right
   (br <- cbind(rbind(m, NA_integer_), NA_integer_))
 
-  exy_values <- colMeans(rbind(c(tl),
+  evals <- colMeans(rbind(c(tl),
                                c(tr),
                                c(bl),
                                c(br)), na.rm = TRUE)
 
-  cbind(exy, exy_values)
+  evals
+}
+
+
+as_mesh3d <- function(x, ...) {
+  nc1 <- ncol(x) + 1
+  aa <- t(prs(seq(nc1)))
+  ind <- matrix(c(rbind(aa, aa[2:1, ])) + c(0, 0, nc1, nc1), 4)
+  ind0 <- as.integer(as.vector(ind) +
+                       rep(seq(0, length = nrow(x), by = ncol(x) + 1), each = 4 * dim(x)[2L]))
+  ind1 <- matrix(ind0, nrow = 4)
+  rgl::qmesh3d(rbind(t(quadmesh::edgesXY(x)),  exy(x), 1),
+               ind1
+               )
 }
