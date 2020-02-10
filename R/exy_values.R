80380db26153615c365dc67b64465448beab2832
Michael Sumner <mdsumner@gmail.com>
2020-02-10 05:49:40 +0000

    get raster and matrix orientation sorted out

--- R/exy_values.R
+++ R/exy_values.R
@@ -1,6 +1,20 @@
+expand_xy <- function(x, y) {
+  ## fast matrix version of expand.grid
+  cbind(x, rep(y, each = length(x)))
+}
+
+edges_xy <- function(x, ...) {
+  ## corner coordinates from a matrix in 0,nrow 0,ncol
+  dm <- dim(x)
+  xx <- seq(0, dm[1L], length = dm[1L] + 1L)
+  yy <- seq(dm[2L], 0, length = dm[2L] + 1L)
+  expand_xy(x = xx, y = yy)
+}
+
+
 ## from a raster, calculate values at cell corner
 ## allow only 1-layer in-mem rasters, not lazy ones
-exy <- function(x, ...) {
+vxy_raster <- function(x, ...) {
   if (is.numeric(x@data@values) && length(x@data@values) > 0) {
     nr <- x@nrows
     nc <- x@ncols
@@ -9,14 +23,24 @@ exy <- function(x, ...) {
   } else {
     stop("raster is not in-memory, cannot obtain values - use 'raster::readAll(x[[1]])' before conversion to mesh")
   }
+  vxy(m)
+}
+to_raster <- function(x, ...) {
+  t(x[nrow(x):1, ])
+}
+## from a matrix, calculate values at cell corner
+vxy <- function(x, ...) {
+  dm <- dim(x)
+  nr <- dm[1L]
+  nc <- dm[2L]
   ## top left
-  tl <- cbind(NA_integer_, rbind(NA_integer_, m))
+  tl <- cbind(NA_integer_, rbind(NA_integer_, x))
   ## top right
-  tr <- cbind(NA_integer_, rbind(m, NA_integer_))
+  tr <- cbind(NA_integer_, rbind(x, NA_integer_))
   ## bottom left
-  bl <- cbind(rbind(NA_integer_, m), NA_integer_)
+  bl <- cbind(rbind(NA_integer_, x), NA_integer_)
   ## bottom right
-  br <- cbind(rbind(m, NA_integer_), NA_integer_)
+  br <- cbind(rbind(x, NA_integer_), NA_integer_)
 
  .colMeans(matrix(c(tl, tr, bl, br), 4L, byrow = TRUE),
                   m = 4L, n = (nr + 1L) * (nc + 1L),
@@ -24,7 +48,32 @@ exy <- function(x, ...) {
 }
 
 
+as_mesh3d_matrix <- function(x,...) {
+  v <- vxy(x)
+  exy <- edges_xy(x)
+
+  dm <- dim(x)
+  ## this was developed against raster, so nc is nr ;)
+  nc <- dm[1L]
+  nr <- dm[2L]
+  nc1 <- nc + 1
+  aa <- t(prs(seq_len(nc1)))
+  ind <- matrix(c(rbind(aa, aa[2:1, ])) + c(0, 0, nc1, nc1), 4)
+  ind0 <- as.integer(as.vector(ind) +
+                       rep(seq(0, length = nr, by = nc1), each = 4 * nc))
+  ind1 <- matrix(ind0, nrow = 4)
+  ## for a matrix, we are done
+  ## for raster, we have to apply the extent transformation
+  cols <- viridis::viridis(100)
+  rgl::qmesh3d(rbind(t(exy), v, 1),
+               ind1,
+               material = list(color = cols[scales::rescale(v, to = c(1, 100))])
+  )
+}
+
 as_mesh3d_raster <- function(x, ...) {
+  v <- vxy_raster(x)
+  exy <- quadmesh::edgesXY(x)
   nc <- x@ncols
   nc1 <- nc + 1
   aa <- t(prs(seq(nc1)))
@@ -32,9 +81,9 @@ as_mesh3d_raster <- function(x, ...) {
   ind0 <- as.integer(as.vector(ind) +
                        rep(seq(0, length = nrow(x), by = ncol(x) + 1), each = 4 * dim(x)[2L]))
   ind1 <- matrix(ind0, nrow = 4)
-  v <- exy(x)
+
   cols <- viridis::viridis(100)
-  rgl::qmesh3d(rbind(t(quadmesh::edgesXY(x)),  v, 1),
+  rgl::qmesh3d(rbind(t(exy),  v, 1),
                ind1,
                material = list(color = cols[scales::rescale(v, to = c(1, 100))])
                )
