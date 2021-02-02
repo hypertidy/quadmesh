3659d22a2da16115bac9f4b542320c69d15900ff
Michael Sumner <mdsumner@gmail.com>
2021-02-02 23:30:58 +0000

    fix doc links

--- vignettes/topography.R
+++ vignettes/topography.R
@@ -16,12 +16,3 @@ lltopo$vb[1:3, ] <- t(llh2xyz(t(lltopo$vb[1:3, ]), exag = 50))
 #  ltt <- ltt0 <- quadmesh(lt, lt)
 #  ltt$vb[1:3, ] <- t(llh2xyz(t(ltt$vb[1:3, ])))
 
-## -----------------------------------------------------------------------------
-library(raster)
-plot(etopo, col = palr::bathy_deep_pal(20))
-prj <- "+proj=stere +lat_ts=-71 +lat_0=-90 +lon_0=147 +datum=WGS84"
-mesh_plot(etopo, crs = prj, asp = 1, col = palr::bathy_deep_pal(20))
-xy <- reproj::reproj(xymap, prj, source = 4326)
-lines(xy)
-
-
